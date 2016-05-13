9e8cccae68d0569a729f3e6a8da069bd03469580
didinyah <dylanheitz@hotmail.fr>
2016-05-13 17:53:02 +0200

    changement dans seeds pour qu'il affiche les erreurs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-matieres = Matiere.create([{titre: 'Maths', periode: Date.new(2009,11,26).to_time.to_i}, {titre: 'Histoire', periode: Date.new(2019,11,26).to_time.to_i}])
-Epreuve.create(titre: 'controle1', date_examen: Date.new(2009,11,26), matiere: matieres.first)
-Epreuve.create(titre: 'controle2', date_examen: Date.new(2019,11,26), matiere: matieres.first)
+matieres = Matiere.create!([{titre: 'Maths', periode: Time.new(2002)}, {titre: 'Histoire', periode: Time.new(2002)}])
+Epreuve.create!(titre: 'controle1', date_examen: Date.new(2009,11,26), matiere: matieres.first)
+Epreuve.create!(titre: 'controle2', date_examen: Date.new(2019,11,26), matiere: matieres.first)
