9457b90f5f5b22c58b5e772b0e11e9cac824dabe
Germain <germain_clauss@yahoo.fr>
2016-05-19 16:41:25 +0200

    Ajout de la gem pour les modales de confirm avec boostrap Modification du manage_pending

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,11 +18,4 @@
 //= require moment
 //= require bootstrap-datetimepicker
 //= require moment/fr
-$(document).ready(function(){
-	$('#modaleDeletion').on('show.bs.modal', function (event) {
-	  var button = $(event.relatedTarget);
-	  var id = button.data('id');
-	 	var modal = $(this);
-	  modal.find('#deleteLink').attr('href','/matieres/destroy?id=' + id);
-	});
-});
+//= require data-confirm-modal
