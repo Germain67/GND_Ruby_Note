7e97ced6e9a6e390cd672f61b62523f4e8fae75e
Naik67 <naik.karst@gmail.com>
2016-05-20 15:10:59 +0200

    ajout de test de droits et de messages d'erreur lors de la consultation de matières

--- app/controllers/matieres_controller.rb
+++ app/controllers/matieres_controller.rb
@@ -2,6 +2,8 @@ class MatieresController < ApplicationController
 
   before_action :verif_pending
   before_action :verif_signin
+  before_action :verif_can_manage, only: [:new, :validate_add, :remove_etudiant, :create, :update, :destroy] 
+  before_action :verif_can_view, only: [:show, :index] 
 
   # on vérifie avant toute chose que l'user n'est pas un pending, si c'est le cas, on le redirige à la page d'accueil
   def verif_pending
@@ -20,37 +22,59 @@ class MatieresController < ApplicationController
     end
   end
 
+  def verif_can_manage
+    ability = Ability.new(current_user)
+    if !ability.can? :manage, Matiere
+        redirect_to matieres_path
+      flash[:error] = "Vous n'avez pas la permission de réaliser cette action."
+    end
+  end
+
+  def verif_can_view
+    ability = Ability.new(current_user)
+    if !ability.can? :view, Matiere
+        redirect_to matieres_path
+      flash[:error] = "Vous n'avez pas la permission de réaliser cette action."
+    end
+  end
   ################################## méthodes avec vues
 
   def index
     ability  = Ability.new(current_user)
     if current_user.has_role? "admin"
       @matieres = Matiere.all
-    elsif ability.can? :view, Matiere
+    else
       @matieres = Array.new
       @appartenances = Appartenance.where("user_id = '"+current_user.id.to_s+"'")
       @appartenances.each do |appartenance|
-          matiere = Matiere.find(appartenance.matiere_id)
-          @matieres.push(matiere)
+        matiere = Matiere.find(appartenance.matiere_id)
+        @matieres.push(matiere)
       end
-    else
-      flash[:error] = "Vous n'avez pas le droit de consulter la liste des matières"
-      redirect_to root_path
     end
   end
  
   def show
-    ability  = Ability.new(current_user)
-    if ability.can? :view, Matiere
-      @matiere = Matiere.find(params[:id])
-      @users = Array.new
+    @matiere = Matiere.find_by_id(params[:id])
+    if @matiere
+      authorizeduser = false
       @appartenances = Appartenance.where("matiere_id = '"+ params[:id] +"'")
       @appartenances.each do |appartenance|
+        if appartenance.user_id == current_user.id
+          authorizeduser = true
+        end
+      end
+      if authorizeduser
+        @users = Array.new
+        @appartenances.each do |appartenance|
           user = User.find(appartenance.user_id)
           @users.push(user)
+        end
+      else
+        flash[:error] = "Vous n'êtes pas inscrit à cette matière."
+        redirect_to matieres_path 
       end
     else
-      flash[:error] = "Vous n'avez pas le droit de consulter cette épreuve"
+      flash[:error] = "La matière à laquelle vous essayez d'accéder n'existe pas."
       redirect_to matieres_path
     end
   end
@@ -64,48 +88,34 @@ class MatieresController < ApplicationController
   end
  
   def create
-    ability  = Ability.new(current_user)
-    if ability.can? :manage, Matiere
-      @matiere = Matiere.new(matiere_params)
+    @matiere = Matiere.new(matiere_params)
 
-      if @matiere.save
-        flash[:success] = "Matière créée avec succès."
-        appartenance = Appartenance.new 
-        appartenance.user_id = current_user.id 
-        appartenance.matiere_id = @matiere.id 
-        appartenance.save!
-        redirect_to matieres_path
-      else
-        flash[:error] = "Erreur lors de la création de la matière."
-        redirect_to new_matiere_path
-      end
-    else
-      flash[:error] = "Vous n'avez pas le droit de créer de matière"
+    if @matiere.save
+      flash[:success] = "Matière créée avec succès."
+      appartenance = Appartenance.new 
+      appartenance.user_id = current_user.id 
+      appartenance.matiere_id = @matiere.id 
+      appartenance.save!
       redirect_to matieres_path
+    else
+      flash[:error] = "Erreur lors de la création de la matière."
+      redirect_to new_matiere_path
     end
   end
  
   def update
-    ability  = Ability.new(current_user)
-    if ability.can? :manage, Matiere
-      @matiere = Matiere.find(params[:id])
+    @matiere = Matiere.find(params[:id])
 
-      if @matiere.update_attributes(matiere_params)
-        flash[:success] = "Matière modifiée avec succès."
-        redirect_to matieres_path
-      else
-        flash[:error] = "Erreur lors de la modification de la matière."
-        redirect_to edit_matiere_path(@matiere)
-      end
-    else
-      flash[:error] = "Vous n'avez pas le droit de modifier de matière"
+    if @matiere.update_attributes(matiere_params)
+      flash[:success] = "Matière modifiée avec succès."
       redirect_to matieres_path
+    else
+      flash[:error] = "Erreur lors de la modification de la matière."
+      redirect_to edit_matiere_path(@matiere)
     end
   end
  
   def destroy
-    ability  = Ability.new(current_user)
-    if ability.can? :manage, Matiere
       @matiere = Matiere.find(params[:id])
       if @matiere.destroy
         flash[:success] = "Matière supprimée avec succès."
@@ -114,10 +124,6 @@ class MatieresController < ApplicationController
         flash[:error] = "Erreur lors de la suppression de la matière."
         redirect_to edit_matiere_path(@matiere)
       end
-    else
-      flash[:error] = "Vous n'avez pas le droit de supprimer de matière"
-      redirect_to matieres_path
-    end
   end
 
   def add_etudiant
@@ -137,56 +143,44 @@ class MatieresController < ApplicationController
   end
 
   def validate_add
-    ability  = Ability.new(current_user)
-    if ability.can? :manage, Matiere
-      @matiere = Matiere.find(params[:matiere_id])
-      addeduser = User.find_by_id(params[:user_id])
-      if addeduser
-        appartenance = Appartenance.new 
-        appartenance.user_id = params[:user_id]
-        appartenance.matiere_id = params[:matiere_id]
-        appartenance.save!
-        flash[:success] = "Etudiant ajouté avec succès."
-        redirect_to add_etudiant_matiere_path(@matiere)
-      else
-        flash[:error] = "L'étudiant ajouté n'existe pas."
-        redirect_to show_matiere_path(@matiere)
-      end
+    @matiere = Matiere.find(params[:matiere_id])
+    addeduser = User.find_by_id(params[:user_id])
+    if addeduser
+      appartenance = Appartenance.new 
+      appartenance.user_id = params[:user_id]
+      appartenance.matiere_id = params[:matiere_id]
+      appartenance.save!
+      flash[:success] = "Etudiant ajouté avec succès."
+      redirect_to add_etudiant_matiere_path(@matiere)
     else
-      flash[:error] = "Vous n'avez pas le droit d'ajouter d'étudiants à cette matière."
-      redirect_to matieres_path
+      flash[:error] = "L'étudiant n'existe pas."
+      redirect_to show_matiere_path(@matiere)
     end
   end
  
   def remove_etudiant
-    ability = Ability.new(current_user)
-    if ability.can? :manage, Matiere
-      @matiere = Matiere.find_by_id(params[:matiere_id])
-      if @matiere
-        removeduser = User.find_by_id(params[:user_id])
-        if removeduser
-          if removeduser.has_role? "etudiant"
-            @appartenances = Appartenance.where(matiere_id: params[:matiere_id], user_id: params[:user_id])
-            @appartenances.each do |appartenance|
-              @appartenance.destroy
-            end
-            flash[:success] = "Etudiant retiré avec succès."
-            redirect_to show_matiere_path(@matiere)
-          else
-            flash[:error] = "L'utilisateur que vous désirez retirer n'est pas un étudiant."
-            redirect_to show_matiere_path(@matiere)
+    @matiere = Matiere.find_by_id(params[:matiere_id])
+    if @matiere
+      removeduser = User.find_by_id(params[:user_id])
+      if removeduser
+        if removeduser.has_role? "etudiant"
+          @appartenances = Appartenance.where(matiere_id: params[:matiere_id], user_id: params[:user_id])
+          @appartenances.each do |appartenance|
+            @appartenance.destroy
           end
+          flash[:success] = "Etudiant retiré avec succès."
+          redirect_to show_matiere_path(@matiere)
         else
-          flash[:error] = "L'étudiant que vous essayez de retirer n'existe pas."
+          flash[:error] = "L'utilisateur que vous désirez retirer n'est pas un étudiant."
           redirect_to show_matiere_path(@matiere)
         end
-        @newstudents = User.where("id NOT IN (?)", @students)
       else
-        flash[:error] = "La matière n'existe pas."
-        redirect_to matieres_path
+        flash[:error] = "L'étudiant que vous essayez de retirer n'existe pas."
+        redirect_to show_matiere_path(@matiere)
       end
+      @newstudents = User.where("id NOT IN (?)", @students)
     else
-      flash[:error] = "Vous n'avez pas le droit de retirer d'étudiants à cette matière."
+      flash[:error] = "La matière n'existe pas."
       redirect_to matieres_path
     end
   end
